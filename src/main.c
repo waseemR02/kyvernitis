4fa5838ec158140dce824bb0209c6dd3183b3b94
waseemR02 <waseem.riaz.9999@gmail.com>
2023-08-09 21:34:57 +0530

    Configure pwm in dts

--- src/main.c
+++ src/main.c
@@ -1,30 +1,62 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file Sample app to demonstrate PWM-based LED fade
+ */
+
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
 
-#define LED0_NODE DT_ALIAS(led0)
+static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define NUM_STEPS	50U
+#define SLEEP_MSEC	25U
 
 int main(void)
 {
+	uint32_t pulse_width = 0U;
+	uint32_t step = pwm_led0.period / NUM_STEPS;
+	uint8_t dir = 1U;
 	int ret;
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0)
-	{
+	printk("PWM-based LED fade\n");
+
+	if (!device_is_ready(pwm_led0.dev)) {
+		printk("Error: PWM device %s is not ready\n",
+		       pwm_led0.dev->name);
 		return 0;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0)
-		{
+		ret = pwm_set_pulse_dt(&pwm_led0, pulse_width);
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
 			return 0;
 		}
-		k_msleep(1000);
+
+		if (dir) {
+			pulse_width += step;
+			if (pulse_width >= pwm_led0.period) {
+				pulse_width = pwm_led0.period - step;
+				dir = 0U;
+			}
+		} else {
+			if (pulse_width >= step) {
+				pulse_width -= step;
+			} else {
+				pulse_width = step;
+				dir = 1U;
+			}
+		}
+
+		k_sleep(K_MSEC(SLEEP_MSEC));
 	}
 	return 0;
 }
