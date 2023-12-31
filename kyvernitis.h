5d0e6db925aa0b0f2a3fb059f0815f4807ca470b
waseemR02 <waseem.riaz.9999@gmail.com>
2023-12-31 17:51:18 +0530

    Move pwm_motor def to kyvernitis.h

--- kyvernitis.h
+++ kyvernitis.h
@@ -1,7 +1,14 @@
 /*
 * Kyervnitis helper macros and function declarations
 */
+#include <zephyr/drivers/pwm.h>
 
 #define MAX_ROBOCLAWS 2
 #define MAX_SABERTOOTHS 3
 #define MAX_SERVOS 5
+
+struct pwm_motor {
+	const struct pwm_dt_spec dev_spec;
+	const uint32_t min_pulse;
+	const uint32_t max_pulse;
+};
