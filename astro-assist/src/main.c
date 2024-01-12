b15c1dfbd5e15cb0ef89c14b49bc2c6e803ec5d8
waseemR02 <waseem.riaz.9999@gmail.com>
2024-01-13 00:04:55 +0530

    Astro-Assist: Write main loop
    Write main loop while considering all switch cases
    Refactor code: change switch_1,2 to switches[]
    Change pin configuration and readiness checks accordingly

--- astro-assist/src/main.c
+++ astro-assist/src/main.c
@@ -15,20 +15,23 @@
 #include <kyvernitis.h>
 
 #define L293D_COUNT 4
+#define L293D_LIM_COUNT 2
 
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 CAN_MSGQ_DEFINE(rx_msgq, 10);
 
 /* DT spec for limit switches */
-const struct gpio_dt_spec switch_1 = GPIO_DT_SPEC_GET_BY_IDX(
-						DT_NODELABEL(limitswitches),
-                                                gpios,
-						0);
+const struct gpio_dt_spec switches[L293D_LIM_COUNT] = {
+		GPIO_DT_SPEC_GET_BY_IDX(
+					DT_NODELABEL(limitswitches),
+                                        gpios,
+					0),
+		GPIO_DT_SPEC_GET_BY_IDX(
+					DT_NODELABEL(limitswitches),
+                                        gpios,
+					1)
+};
 
-const struct gpio_dt_spec switch_2 = GPIO_DT_SPEC_GET_BY_IDX(
-						DT_NODELABEL(limitswitches),
-                                                gpios,
-						1);
 
 /* DT spec for all instances of motors controlled by l293d*/
 const struct dc_motor l293d[L293D_COUNT] = {
@@ -76,18 +79,13 @@ int main()
 		LOG_INF("CAN: Device %s not ready.\n", can_dev->name);
 		return 0;
 	}
-
-	if (!gpio_is_ready_dt(&switch_1))
-	{
-		LOG_ERR("Error: Led not ready\n");
-		return 0;
+	
+	for (size_t i = 0U; i < ARRAY_SIZE(switches); i++) {
+		if (!gpio_is_ready_dt(&(switches[i]))) {
+			LOG_ERR("Limit Switch: %d is not ready\n", i + L293D_BASE_ID);
+			return 0;
+		}
 	}
-
-	if (!gpio_is_ready_dt(&switch_2))
-	{
-		LOG_ERR("Error: Led not ready\n");
-		return 0;
-	};
 
 	for (size_t i = 0U; i < ARRAY_SIZE(l293d); i++) {
 		if (!gpio_is_ready_dt(&(l293d[i].input_1))) {
@@ -103,20 +101,16 @@ int main()
 	}
 
 	/* Start up configurations */
-
-	err = gpio_pin_configure_dt(&switch_1, GPIO_INPUT);
-	if (err != 0) {
-		LOG_ERR("Error %d: failed to configure %s pin %d\n",
-		       err, switch_1.port->name, switch_1.pin);
-		return 0;
+	
+	for (size_t i = 0U; i < ARRAY_SIZE(switches); i++) {
+		err = gpio_pin_configure_dt(&switches[i], GPIO_INPUT);
+		if (err != 0) {
+			LOG_ERR("Error %d: failed to configure %s pin %d\n",
+					err, switches[i].port->name, switches[i].pin);
+			return 0;
+		}
 	}
 
-	err = gpio_pin_configure_dt(&switch_2, GPIO_INPUT);
-	if (err != 0) {
-		LOG_ERR("Error %d: failed to configure %s pin %d\n",
-		       err, switch_2.port->name, switch_2.pin);
-		return 0;
-	}
 
 #ifdef CONFIG_LOOPBACK_MODE
 	if (can_set_mode(can_dev, CAN_MODE_LOOPBACK)) {
@@ -138,4 +132,46 @@ int main()
 	}
 
 	LOG_INF("Initialization completed successfully\n");
+
+	while (true)
+	{
+		k_msgq_get(&rx_msgq, &astro_assist_rx_frame, K_FOREVER);
+
+		struct can_frame frame = astro_assist_rx_frame;
+		if(frame.dlc != 6)
+		{
+			//just handling motor commands for now
+			LOG_ERR("Unknown Frame Received\n");
+			continue;
+		}
+
+		switch (frame.data[4]) {
+		
+		case ACTUATOR_COMMAND_ID:
+			if (frame.data[5] < L293D_BASE_ID + L293D_LIM_COUNT)
+			{
+				// Consider from 25 to 26
+				int motor_no = frame.data[5] - L293D_BASE_ID;
+				err = dc_motor_write_lim(&l293d[motor_no],
+							frame.data_32[0],
+							&switches[motor_no]);
+				if (err)
+				{
+					return 0;
+				}
+			}	
+			else if (frame.data[4] < L293D_BASE_ID + L293D_COUNT)
+			{
+				// consider from 25 to 28	
+				err = dc_motor_write(&l293d[frame.data[5] - L293D_BASE_ID],
+							frame.data_32[0]);
+				
+				if (err)
+				{
+					return 0;
+				}
+			}
+			break;
+		}
+	}
 }
