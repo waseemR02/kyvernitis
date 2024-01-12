2e27a845a7fa22a05b80134c8f705449f50e48d7
waseemR02 <waseem.riaz.9999@gmail.com>
2024-01-12 10:23:43 +0530

    Astro-Assist: add new struct to hold dc motor instance

--- kyvernitis.h
+++ kyvernitis.h
@@ -3,5 +3,10 @@
 */
 
 #define MAX_ROBOCLAWS 2
-#define MAX_SABERTOOTHS 3
+#define MAX_SABERTOOTHS 2
 #define MAX_SERVOS 5
+
+struct dc_motor {
+	const struct gpio_dt_spec input_1;
+	const struct gpio_dt_spec input_2;
+};
