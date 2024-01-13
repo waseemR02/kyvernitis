b1db152597f5d30a1c14ece74e361dbd99eeee76
Mohnish4533 <mv4533@srmist.edu.in>
2024-01-13 21:20:24 +0530

    modify: LED defined, code refactored, enabled CAN

--- fpv_switch/src/main.c
+++ fpv_switch/src/main.c
@@ -1,22 +1,13 @@
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
-
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/can.h>
-
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/sys/util.h>
-
+#include <zephyr/logging/log.h>
 #include <app_version.h>
 
 #include <kycan.h>
-
-#include <zephyr/logging/log.h>
-
 
 #ifdef CONFIG_LOOPBACK_MODE
 	#define TX_THREAD_STACK_SIZE 512
@@ -26,16 +17,17 @@
 
 #endif
 
-
-CAN_MSGQ_DEFINE(rx_msgq, 10);
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
-
+CAN_MSGQ_DEFINE(rx_msgq, 10);
 
 /* DT spec for gpio pins */
 const struct gpio_dt_spec gpio_1 = GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(io_pins), gpios, 0);
 const struct gpio_dt_spec gpio_2 = GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(io_pins), gpios, 1);
 const struct gpio_dt_spec gpio_3 = GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(io_pins), gpios, 2);
 const struct gpio_dt_spec gpio_4 = GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(io_pins), gpios, 3);
+
+/* DT spec for led*/
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 /* DT spec for can module */
 const struct device *const can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
@@ -80,33 +72,66 @@ struct can_frame fpv_switch_rx_frame;
 
 int main(void)
 {
+	printk("\nFPV-Switch: v%s\n\n", APP_VERSION_STRING);
+	
+	int err;
+
+	/* Device ready checks */
+
 	if (!gpio_is_ready_dt(&gpio_1)) {
-        	printf("GPIO 1 : Pin not ready\n");
-        } else {
-                printf("GPIO 1 : Pin ready\n");
-        };
+		LOG_ERR("GPIO 1 : Pin not ready\n");
+		return 0;
+    };
 
+	if (!gpio_is_ready_dt(&gpio_2)) {
+		LOG_ERR("GPIO 2 : Pin not ready\n");
+		return 0;
+    };
+	
+	if (!gpio_is_ready_dt(&gpio_3)) {
+		LOG_ERR("GPIO 3 : Pin not ready\n");
+		return 0;
+    };
 
-        if (!gpio_is_ready_dt(&gpio_2)) {
-                printf("GPIO 2 : Pin not ready\n");
-        } else {
-                printf("GPIO 2 : Pin ready\n");
-        };
+	if (!gpio_is_ready_dt(&gpio_4)) {
+		LOG_ERR("GPIO 4 : Pin not ready\n");
+		return 0;
+    };
 
+	if (!device_is_ready(can_dev)) {
+		LOG_ERR("CAN: Device %s not ready.\n", can_dev->name);
+		return 0;
+	}
 
-        if (!gpio_is_ready_dt(&gpio_3)) {
-                printf("GPIO 3 : Pin not ready\n");
-        } else {
-                printf("GPIO 3 : Pin ready\n");
-        };
+	/* Start up configurations */
 
+	err = gpio_pin_configure_dt(&gpio_1, GPIO_OUTPUT_INACTIVE);
+	if (err != 0) {
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				err, gpio_1.port->name, gpio_1.pin);
+		return 0;
+	}
 
-        if (!gpio_is_ready_dt(&gpio_4)) {
-                printf("GPIO 4 : Pin not ready\n");
-        } else {
-                printf("GPIO 4 : Pin ready\n");
-        };
-	k_msleep(3000);
+	err = gpio_pin_configure_dt(&gpio_2, GPIO_OUTPUT_INACTIVE);
+	if (err != 0) {
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				err, gpio_2.port->name, gpio_2.pin);
+		return 0;
+	}
+
+	err = gpio_pin_configure_dt(&gpio_3, GPIO_OUTPUT_INACTIVE);
+	if (err != 0) {
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				err, gpio_3.port->name, gpio_3.pin);
+		return 0;
+	}
+
+	err = gpio_pin_configure_dt(&gpio_4, GPIO_OUTPUT_INACTIVE);
+	if (err != 0) {
+		LOG_ERR("Error %d: failed to configure %s pin %d\n",
+				err, gpio_4.port->name, gpio_4.pin);
+		return 0;
+	}
 
 
 #ifdef CONFIG_LOOPBACK_MODE
@@ -138,9 +163,28 @@ int main(void)
 		LOG_ERR("ERROR spawning tx thread\n");
 	}
 
+#endif
+
+	if (can_start(can_dev)) {
+		LOG_ERR("Error starting CAN controller.\n");
+		return 0;
+	}
+
+	int filter_id = can_add_rx_filter_msgq(can_dev, &rx_msgq, &fpv_filter);
+	if (filter_id < 0)
+	{
+		LOG_ERR("Unable to add rx msgq [%d]", filter_id);
+		return 0;
+	}
+
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (err != 0) {
+		LOG_ERR("Error : LED not configured\n");
+		return 0;
+	}
+
 	LOG_INF("Initialization completed successfully\n");
 
-#endif
 
 	while(1) {
 
@@ -150,32 +194,33 @@ int main(void)
 
 		switch (frame.data[5]) {	
 			
-			case '0':
-				gpio_pin_set_dt(&gpio_1, 1);
-				gpio_pin_set_dt(&gpio_2, 0);
-				gpio_pin_set_dt(&gpio_3, 0);
-				gpio_pin_set_dt(&gpio_4, 0);
-				break;
-			case '1':
-                                gpio_pin_set_dt(&gpio_1, 0);
-                                gpio_pin_set_dt(&gpio_2, 1);
-                                gpio_pin_set_dt(&gpio_3, 0);
-                                gpio_pin_set_dt(&gpio_4, 0);
-				break;
-			case '2':
-                                gpio_pin_set_dt(&gpio_1, 0);
-                                gpio_pin_set_dt(&gpio_2, 0);
-                                gpio_pin_set_dt(&gpio_3, 1);
-                                gpio_pin_set_dt(&gpio_4, 0);
-                                break;
-			case '3':
-                                gpio_pin_set_dt(&gpio_1, 0);
-                                gpio_pin_set_dt(&gpio_2, 0);
-                                gpio_pin_set_dt(&gpio_3, 0);
-                                gpio_pin_set_dt(&gpio_4, 1);
-                                break;
-	
+		case 0:
+			gpio_pin_set_dt(&gpio_1, 1);
+			gpio_pin_set_dt(&gpio_2, 0);
+			gpio_pin_set_dt(&gpio_3, 0);
+			gpio_pin_set_dt(&gpio_4, 0);
+			break;
+		case 1:
+            gpio_pin_set_dt(&gpio_1, 0);
+            gpio_pin_set_dt(&gpio_2, 1);
+            gpio_pin_set_dt(&gpio_3, 0);
+            gpio_pin_set_dt(&gpio_4, 0);
+			break;
+		case 2:
+            gpio_pin_set_dt(&gpio_1, 0);
+            gpio_pin_set_dt(&gpio_2, 0);
+            gpio_pin_set_dt(&gpio_3, 1);
+            gpio_pin_set_dt(&gpio_4, 0);
+            break;
+		case 3:
+            gpio_pin_set_dt(&gpio_1, 0);
+            gpio_pin_set_dt(&gpio_2, 0);
+            gpio_pin_set_dt(&gpio_3, 0);
+            gpio_pin_set_dt(&gpio_4, 1);
+            break;
 		}
+
+		gpio_pin_toggle_dt(&led);
 	}
 	return 0;
 }
