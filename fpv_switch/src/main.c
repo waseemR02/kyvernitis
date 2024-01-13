ce19abcfd1f797d1a0135bf4fe12b5bdb43fbdf1
Mohnish4533 <mv4533@srmist.edu.in>
2024-01-13 15:46:30 +0530

    TESTING: gpio-uart transmission

--- fpv_switch/src/main.c
+++ fpv_switch/src/main.c
@@ -15,6 +15,9 @@
 #include <zephyr/logging/log.h>
 
 
+#define SLEEP_TIME_MS   1000
+
+
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 
@@ -30,32 +33,35 @@ const struct gpio_dt_spec gpio_4 = GPIO_DT_SPEC_GET_BY_IDX(DT_NODELABEL(io_pins)
 
 int main(void)
 {
-	if (!gpio_is_ready_dt(&gpio_1)) {
-		printf("GPIO 1 : Pin not ready\n");
-	} else {
-		printf("GPIO 1 : Pin ready\n");
-	};
+	while(1) {
+		if (!gpio_is_ready_dt(&gpio_1)) {
+			printf("GPIO 1 : Pin not ready\n");
+		} else {
+			printf("GPIO 1 : Pin ready\n");
+		};
 
 
-	if (!gpio_is_ready_dt(&gpio_2)) {
-                printf("GPIO 2 : Pin not ready\n");
-        } else {
-                printf("GPIO 2 : Pin ready\n");
-        };
+		if (!gpio_is_ready_dt(&gpio_2)) {
+                	printf("GPIO 2 : Pin not ready\n");
+        	} else {
+                	printf("GPIO 2 : Pin ready\n");
+        	};
 
 
-	if (!gpio_is_ready_dt(&gpio_3)) {
-                printf("GPIO 3 : Pin not ready\n");
-        } else {
-                printf("GPIO 3 : Pin ready\n");
-        };
+		if (!gpio_is_ready_dt(&gpio_3)) {
+                	printf("GPIO 3 : Pin not ready\n");
+        	} else {
+                	printf("GPIO 3 : Pin ready\n");
+        	};
 
 
-	if (!gpio_is_ready_dt(&gpio_4)) {
-                printf("GPIO 4 : Pin not ready\n");
-        } else {
-                printf("GPIO 4 : Pin ready\n");
-        };
+		if (!gpio_is_ready_dt(&gpio_4)) {
+                	printf("GPIO 4 : Pin not ready\n");
+        	} else {
+                	printf("GPIO 4 : Pin ready\n");
+        	};
+		k_msleep(SLEEP_TIME_MS);
+	}
 
 	return 0;
 }
