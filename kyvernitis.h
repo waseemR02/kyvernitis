e0290a2f90989787b119c881ad1edc5e4c0e89b0
waseemR02 <waseem.riaz.9999@gmail.com>
2024-01-17 23:14:24 +0530

    Gripper-Arm: define stepper_motor_write and stepper motor macros

--- kyvernitis.h
+++ kyvernitis.h
@@ -3,6 +3,7 @@
 */
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 
 #define MAX_ROBOCLAWS 2
 #define MAX_SABERTOOTHS 3
@@ -10,6 +11,9 @@
 
 #define PWM_MOTOR_STOP 1520000
 #define SERVO_DEFAULT_STATE 1520000
+
+#define STEPPER_MOTOR_FORWARD 1
+#define STEPPER_MOTOR_BACKWARD 2
 
 struct pwm_motor {
 	const struct pwm_dt_spec dev_spec;
@@ -27,6 +31,8 @@ typedef uint32_t servo_state_t;
 // Wrapper around pwm_set_pulse_dt to ensure that pulse_width
 // remains under max-min range
 int pwm_motor_write(const struct pwm_motor *motor, uint32_t pulse_width);
+
+int stepper_motor_write(const struct stepper_motor *motor, uint8_t cmd);
 
 float MQ2_readings(int mv);
 
