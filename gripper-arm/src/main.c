16ea213c1dd5e5a5046a6f6ac7a1253262a070f4
waseemR02 <waseem.riaz.9999@gmail.com>
2024-01-17 23:48:46 +0530

    Gripper-Arm: Send motor cmds to stepper motor

--- gripper-arm/src/main.c
+++ gripper-arm/src/main.c
@@ -205,23 +205,27 @@ int main()
 		switch (frame.data[4]) {
 		
 		case ACTUATOR_COMMAND_ID:
-			if (frame.data[5] < ROBOCLAW_BASE_ID + ROBOCLAWS_COUNT)
-			{
+			if (frame.data[5] < ROBOCLAW_BASE_ID + ROBOCLAWS_COUNT) {
 				// in the case it will only consider from 10 - 11
 				if (pwm_motor_write(&roboclaw[frame.data[5] - ROBOCLAW_BASE_ID], frame.data_32[0])) {
 					LOG_ERR("Unable to write pwm pulse to PWM motor: %d", frame.data[5]);
 					return 0;
 				}
 			}
-			else if (frame.data[5] < SERVO_BASE_ID)
-			{
-				// it will consider from 15 - 19
+			else if (frame.data[5] == 17) {
+				// it will consider from 17
 				if(pwm_motor_write(&servo, servo_state)) {
 					LOG_ERR("Unable to write pwm pulse to Servo Motor: %d", frame.data[5]);
 					return 0;
 				}
 				else {
 					servo_state = frame.data_32[0];
+				}
+			}
+			else if (frame.data[5] < STEPPER_BASE_ID + STEPPER_MOTOR_COUNT) {
+				if(stepper_motor_write(&tb6600[frame.data[5] - STEPPER_BASE_ID], frame.data_32[0])) {
+					LOG_ERR("Unable to write motor command to Stepper: %d", frame.data[5]);
+					return 0;
 				}
 			}
 			break;
